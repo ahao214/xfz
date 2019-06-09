3226e8ce8a30eb722b728bb7048cd2cbb86676b9
zhang-derek <1184405959@qq.com>
2019-06-09 16:52:59 +0800

    1.项目环境搭建

---
+++ README.md
@@ -0,0 +1,11 @@
+
+# xfz
+Django大型企业官网
+## MyBlog
+博客里面有这个项目的详细教程
+[点此进入我的博客](https://www.cnblogs.com/derek1184405959/category/1479033.html)<br />
+## 环境
+python: 3.6.4<br /> 
+Django: 2.0.2
+
+
---
+++ front/gulpfile.js
@@ -0,0 +1,84 @@
+var gulp = require("gulp");
+var cssnano = require("gulp-cssnano");
+var rename = require("gulp-rename");
+var uglify = require("gulp-uglify");
+var concat = require("gulp-concat");
+var tinypng_nokey = require('gulp-tinypng-nokey');
+var bs = require("browser-sync").create();
+var sass = require("gulp-sass");
+
+
+
+//html任务
+gulp.task("html",done =>{
+	gulp.src("./templates/**/*.html")
+    .pipe(bs.reload({
+		stream: true
+	}));
+	done();
+});
+
+
+// css任务
+gulp.task("css",done =>{
+	gulp.src("./src/css/*.scss")
+	.pipe(sass().on("error",sass.logError))
+	.pipe(cssnano())   //压缩
+	.pipe(rename({"suffix":".min"}))  //加后缀名
+	.pipe(gulp.dest('./dist/css/'))   //压缩后存放的路径
+    .pipe(bs.reload({
+		stream: true
+	}));
+	done();
+});
+
+//js任务
+gulp.task("js",done =>{
+	gulp.src("./src/js/*.js")
+	.pipe(uglify({
+		'toplevel':true,
+		'compress':{
+			'drop_console':true
+		}
+	}))
+	.pipe(rename({"suffix":".min"}))
+	.pipe(gulp.dest('./dist/js/'))
+	.pipe(bs.reload({
+		stream: true
+	}));
+	done();
+});
+
+//images任务
+gulp.task("images",done =>{
+	gulp.src("./src/images/*.*")
+	.pipe(tinypng_nokey())
+	.pipe(gulp.dest('./dist/images/'))
+	.pipe(bs.reload({
+		stream: true
+	}));
+	done();
+});
+
+//监听
+gulp.task('auto',function () {
+    gulp.watch("./templates/**/*.html",gulp.series('html'));
+    gulp.watch("./src/css/*.scss",gulp.series('css'));
+    gulp.watch("./src/js/*.js",gulp.series('js'));
+    gulp.watch("./src/images/*.*",gulp.series('images'));
+});
+
+//初始化
+gulp.task("bs",function () {
+	bs.init({
+		'server':{
+			'baseDir':'./',
+		}
+	})
+});
+
+//创建一个默认任务
+gulp.task("default",gulp.parallel('bs','auto'));
+
+
+
