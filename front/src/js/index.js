fa1b7b408cbdf64b00bd8eb6d6bd787f6761b599
zhang-derek <1184405959@qq.com>
2019-06-15 16:49:38 +0800

    7.鼠标控制轮播图暂停和继续

--- front/src/js/index.js
+++ front/src/js/index.js
@@ -1,18 +1,42 @@
 //初始化
 function Banner() {
+    this.bannerGroup = $("#banner-group");
+    this.index = 0;
+    this.listenBannerHover();
+};
 
+Banner.prototype.listenBannerHover = function (){
+  var self = this;
+  this.bannerGroup.hover(function () {
+      //鼠标移动到上面
+      clearInterval(self.timer);
+  },function () {
+      //鼠标离开
+      self.loop();
+  });
+};
+
+Banner.prototype.loop = function(){
+    var self = this;
+    var bannerUL = $("#banner-ul");
+    this.timer = setInterval(function () {
+        if(self.index >= 3){
+            self.index = 0
+        }else{
+            self.index++;
+        }
+        bannerUL.animate({"left":-798*self.index},500);
+    },2000);
 };
 
 //添加一个run方法
 Banner.prototype.run = function () {
-    var bannerUL = $("#banner-ul");
-    //500是间隔时间0.5s
-    bannerUL.animate({"left":-798},500)
+    this.loop();
 };
 
 //页面加载完成后执行。创建一个对象，运行方法
 $(function () {
     var banner = new Banner();
-    banner.run()
+    banner.run();
 });
 
